3f055fde4ef0ed9d55863782cbfcc2034f0637f6
rafi.saeful <rafi.saeful@gmail.com>
2025-02-01 12:27:09 +0700

    first commit

---
+++ auth-app/src/main/java/com/dungeon/meshi/senshi/AuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.dungeon.meshi.senshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthAppApplication.class, args);
+  }
+
+}
